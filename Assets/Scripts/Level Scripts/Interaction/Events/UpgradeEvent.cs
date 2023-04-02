343e6e2e970d862006c9f13887f7afb9825968c2
Christopher Tye <ctye@unm.edu>
2023-04-01 19:04:45 -0600

    Fixed upgrade event raise

--- Assets/Scripts/Level Scripts/Interaction/Events/UpgradeEvent.cs
+++ Assets/Scripts/Level Scripts/Interaction/Events/UpgradeEvent.cs
@@ -5,10 +5,10 @@ using System;
 [CreateAssetMenu(menuName = "InteractionEvents/Upgrade")]
 public class UpgradeEvent : InteractionEvent
 {
-    public static event Action<PlayerController, InteractionHandler> OnTowerUpgrade;
-
+    public static event Action<PlayerController, InteractionHandler, int> OnTowerUpgrade;
+    [SerializeField] private int upgradeIndex;
     public override void Raise(PlayerController player, InteractionHandler handler)
     {
-        OnTowerUpgrade?.Invoke(player, handler);
+        OnTowerUpgrade?.Invoke(player, handler, upgradeIndex);
     }
 }
